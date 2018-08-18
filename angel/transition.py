0b0633c8cc0a8665b01440fbd7e0443e6f2e308d
Andy Yen <38731840+onlyjackfrost@users.noreply.github.com>
2018-08-18 22:33:59 +0800

    Add files via upload

--- angel/transition.py
+++ angel/transition.py
@@ -40,12 +40,20 @@ def cost_parser(detail):
     """
         parse the duration and cost for the transit
     """
-    text = detail["routes"][0]["fare"]["text"][1:-3]
-    #[x for x in xyz if x in a]
-    for step in detail["routes"][0]["legs"][0]["steps"]:
-        if step["travel_mode"] == "TRANSIT":
-            duration =step["duration"]["text"]
-    cost = [text,duration]
+    text = "0"
+    duration = "不知道多久"
+    try:
+        text = detail["routes"][0]["fare"]["text"][1:-3]
+    except KeyError:
+        text = "0"
+    try:
+        for step in detail["routes"][0]["legs"][0]["steps"]:
+            if step["travel_mode"] == "TRANSIT":
+                duration =step["duration"]["text"]
+    except KeyError:
+        duration = "不知道多久"
+    finally :
+        cost = [text,duration]
     return cost
 
 def bus_detail_parser(bus_detail):
@@ -66,7 +74,7 @@ def bus_detail_parser(bus_detail):
             shortname = []
     #write file
     with open("bus_detail.txt",'w')as f:
-        if not shortname:
+        if shortname==[]:
             f.write("距離很近，不用搭公車")
         else:
             f.write("請從"+departure_stop+"站，搭"+shortname+"公車到，"+arrival_stop+"站下車")
@@ -78,6 +86,7 @@ def tram_detail_parser(bus_detail):
         parse the detail information for the transit
         上車站、下車站、捷運線
     """
+    shortname = []
     #get route detail
     for index in range(0,len(bus_detail["routes"][0]["legs"][0]["steps"])):
         step = bus_detail["routes"][0]["legs"][0]["steps"][index]
@@ -90,7 +99,7 @@ def tram_detail_parser(bus_detail):
             shortname = []
     #write file
     with open("tram_detail.txt",'w')as f:
-        if not shortname:
+        if shortname==[]:
             f.write("距離很近，不用搭捷運")
         else:
             f.write("請從"+departure_stop+"，搭"+shortname+"到，"+arrival_stop+"下車")
@@ -107,10 +116,10 @@ def bus_algorithm(destination):
     user_location = Geolocation_request()
     #get the transit recommandation
     bus_detail = direction_request(user_location,destination,transit_mode)
-    #parse the cost and time 
-    cost = cost_parser(bus_detail)
     #parse for the detail transit information
     bus_shortname = bus_detail_parser(bus_detail)
+    #parse the cost and time 
+    cost = cost_parser(bus_detail)
     return cost, bus_detail, bus_shortname
 
 def tram_algorithm(destination):
@@ -121,10 +130,10 @@ def tram_algorithm(destination):
     user_location = Geolocation_request()
     #get the transit recommandation
     tram_detail = direction_request(user_location,destination,transit_mode)
-    #parse the cost and time 
-    cost = cost_parser(tram_detail)
     #parse for the detail transit information
     tram_shortname = tram_detail_parser(tram_detail)
+    #parse the cost and time 
+    cost = cost_parser(tram_detail)
     return cost, tram_detail, tram_shortname
 
 def transition(destination):
@@ -133,12 +142,14 @@ def transition(destination):
     with open("transition_cost.txt","w") as f:
         if not tram_shortname or not bus_shortname:
             f.write("距離很近，你問人然後走路吧")
+            out = 0
         if bus_shortname and tram_shortname:
             f.write("搭捷運要"+tram_cost[1]+','+tram_cost[0]+"元"+","
                     "搭公車要"+bus_cost[1]+","+bus_cost[0]+"元"+
-                    "你要搭捷運還是公車")
-    return bus_detail, tram_detail, bus_shortname, tram_shortname
+                    "你要搭捷運、公車、")
+            out = 1
+    return  out
         
 if __name__ == "__main__":
-    destination = "台北車站"
-    bus_detail, tram_detail, bus_shortname, tram_shortname = transition(destination)
+    destination = "我"
+    out = transition(destination)
