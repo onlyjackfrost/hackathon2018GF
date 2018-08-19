8c15687cf06cf3cb455279e26d5813b55ea5589e
Andy Yen <38731840+onlyjackfrost@users.noreply.github.com>
2018-08-19 08:13:01 +0800

    load weather.txt

--- angel/weather.py
+++ angel/weather.py
@@ -5,6 +5,7 @@ Created on Sun Aug 19 07:27:20 2018
 @author: user
 """
 import pandas as pd
+import json
 
 
 def sum_rain(df, town):
@@ -18,17 +19,18 @@ def sum_rain(df, town):
     return avg_rain
 
 def weather():
-    df = pd.read_csv("rain.csv",encoding='big5')
-    town_name = df["town_name"].unique()
+    with open("weather.txt",'r') as f:
+        content = f.read()
+        dict_avg_rain = json.loads(content)
     
-    dict_avg_rain=dict()
-    for town in town_name:
-        avg_rain = sum_rain(df,town)
-        if avg_rain > 4:
-            broacast = "提醒您今天目的地可能會下雨"
-        else :
-            broacast = "提醒您今天目的地不會下雨"
-        dict_avg_rain[town] = broacast
+#    dict_avg_rain=dict()
+#    for town in town_name:
+#        avg_rain = sum_rain(df,town)
+#        if avg_rain > 4:
+#            broacast = "提醒您今天目的地可能會下雨"
+#        else :
+#            broacast = "提醒您今天目的地不會下雨"
+#        dict_avg_rain[town] = broacast
     return dict_avg_rain
 if __name__ == "__main__":
     dict_avg_rain = weather()
